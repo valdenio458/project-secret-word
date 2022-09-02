a326c2675e17031dc5081e8dc804590f854ce2fb
valdenio458 <valdeniomartinho@gmail.com>
2022-09-02 12:18:59 -0300

    Adiciona fruta

--- src/data/words.js
+++ src/data/words.js
@@ -1,6 +1,6 @@
 export const wordsList = {
   carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
-  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Melancia"],
   corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
   computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
   programação: ["Linguagem", "Framework", "JavaScript", "React"],
